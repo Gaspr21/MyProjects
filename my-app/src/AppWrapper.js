7eb2ce8910f2e757c8689d87f4ce78a5ede97a55
Gašpr <96392498+Gaspr21@users.noreply.github.com>
2025-07-22 19:47:17 +0200

    asd
    asd

---
+++ my-app/src/AppWrapper.js
@@ -0,0 +1,12 @@
+import './style/App.css';
+import App from './App';
+
+function AppWrapper() {
+  return (
+    <div className="App-Wrapper">
+      <App/>
+    </div>
+  );
+}
+
+export default AppWrapper;
